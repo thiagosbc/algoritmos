3e7a3a042043813096450439599838b2f6bbce51
thiagosbc <thiagosbc@dcc.ufrj.br>
2025-11-15 20:32:10 -0300

    Removendo arquivos da IDE e adicionando código de algoritmos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import buscabinaria.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("(**Testando algoritmo de Busca Binária**)");
+        int[] vetor = {1,2,3,4,5,6,7,8,9,10};
+        PesquisaBinaria.pesquisa_binaria(vetor, 5);
+        PesquisaBinaria.pesquisa_binaria(vetor, 42);
+        PesquisaBinaria.pesquisa_binaria(vetor, 2);
+
+
+
+
+
+
     }
+
 }
--- src/buscabinaria/PesquisaBinaria.java
+++ src/buscabinaria/PesquisaBinaria.java
@@ -1,4 +1,27 @@
 package buscabinaria;
-
 public class PesquisaBinaria {
+
+    public static void pesquisa_binaria(int[] array, int valor) {
+
+        int inicio = 0;
+        int fim = array.length - 1;
+
+        while (inicio <= fim) {
+
+            int meio = (inicio + fim) / 2;
+
+            if (array[meio] == valor) {
+                System.out.println("Valor encontrado na índice: " + meio);
+                return;
+            }
+
+            if (array[meio] < valor) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+
+        System.out.println("O valor não está na lista.");
+    }
 }
